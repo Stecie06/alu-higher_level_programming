68563ca5630871f2c6bec3024a79fdec1bdef0b8
Stecie06 <n.stecie@alustudent.com>
2024-03-24 21:20:06 +0000

    Network 1

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Documented now"""
-import requests
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
 import sys
+import requests
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    value = sys.argv[2]
-    response = requests.post(url, data={"email": value})
-    print("{}".format(response.text))
+if __name__ == "__main__":
+
+    new_value = {'email': sys.argv[2]}
+    req = requests.post(sys.argv[1], new_value)
+    print(req.text)
--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Documented now"""
-import requests
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
 import sys
+import requests
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        params = sys.argv[1]
-    except IndexError:
-        params = ""
-    response = requests.post(
-        "http://0.0.0.0:5000/search_user",
-        data={"q": params}
-    )
-    try:
-        json_response = response.json()
-        if response.headers.get("Content-Type") == 'application/json':
-            if len(json_response) > 0:
-                print("[{}] {}".format(
-                    json_response["id"],
-                    json_response["name"])
-                )
-            else:
-                print("No result")
-    except:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print("Not a valid JSON")
